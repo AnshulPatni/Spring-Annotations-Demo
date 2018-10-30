c3fc8f1cd3e05adfe423f9fae1b891f8424e61d0
AnshulPatni <anshulpatni19@gmail.com>
2018-10-29 23:44:27 -0700

    Minor fix

--- src/com/anshul/springdemo/TennisCoach.java
+++ src/com/anshul/springdemo/TennisCoach.java
@@ -2,7 +2,7 @@ package com.anshul.springdemo;
 
 import org.springframework.stereotype.Component;
 
-@Component("thatSillyCoach")
+@Component
 public class TennisCoach implements Coach {
 
 	@Override
