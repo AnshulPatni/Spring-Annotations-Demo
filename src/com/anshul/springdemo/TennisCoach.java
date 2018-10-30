5274443446bcd05c962d71cb5aad269b184c5517
AnshulPatni <anshulpatni19@gmail.com>
2018-10-30 00:20:07 -0700

    @Autowired Setter Injection

--- src/com/anshul/springdemo/TennisCoach.java
+++ src/com/anshul/springdemo/TennisCoach.java
@@ -8,9 +8,23 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	// constructor injection
+	/*
 	@Autowired
 	public TennisCoach(FortuneService theFortuneService) {
 		fortuneService = theFortuneService;
+	}
+	*/
+	
+	public TennisCoach() {
+		System.out.println(">> TennisCoach: inside default constructor");
+	}
+	
+	// define a setter method to do the setter injection
+	@Autowired
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">> TennisCoach: inside setter injection method setFortuneService()");
+		this.fortuneService = fortuneService;
 	}
 	
 	@Override
